1fb45e8eb0b3ec52b116ebe2241ee3e9023828a1
P1T1B0Y98 <piti-jo@live.no>
2022-10-18 13:17:28 +0200

    Pages added

---
+++ frontend/src/components/Pages/ProfilePage.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'solid-js';
+
+const ProfilePage: Component = () => {
+    return (
+        <div>
+            <h2>ProfilePage</h2>
+        </div>
+    )
+}
+
+export default ProfilePage;
