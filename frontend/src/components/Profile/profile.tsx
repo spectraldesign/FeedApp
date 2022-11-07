db0dc2724f9fe2b31099839b732c21daff5be768
jolsaker98 <111968598+jolsaker98@users.noreply.github.com>
2022-11-08 00:11:03 +0100

    Get user information for profile page

--- frontend/src/components/Profile/profile.tsx
+++ frontend/src/components/Profile/profile.tsx
@@ -1,8 +1,80 @@
 import { createSignal, createEffect, createMemo } from "solid-js";
 // import { css } from 'emotion';
 import "./profile.css";
+import { useNavigate, NavLink } from '@solidjs/router';
+
+interface Tester {
+    fullNameTest: string;
+    userNameTest: string;
+    emailTest: string;
+}
 
 function Profile() {
+
+    const [fullName, setFullName] = createSignal('');
+    setFullName('Full name');
+    const [userName, setUserName] = createSignal('');
+    setUserName('Username');
+    const [email, setEmail] = createSignal('');
+    setEmail('Email');
+    const [votes, setVotes] = createSignal(0);
+
+    const authentic = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiam9sa29ob2wiLCJleHAiOjE2Njc4NjI1NTcsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjcyNzEvIiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NzI3MS8ifQ.37CW0qtNXcss-3JbSTsGyhgQ5fI9RFLL0yxxNQ4yBfQ'
+    // const tester = localStorage.getItem("token");
+
+    const navigate = useNavigate();
+    fetch('https://localhost:7280/api/user/self', {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Authorization': 'Bearer ' + authentic,
+                },
+                
+                // body: JSON.stringify(data)
+            })
+            .then(response => {
+                if (response.status === 200) {
+                    console.log(response);
+                    // alert('Get request success');
+                    return response.json();
+                } else {
+                    alert('Get request success invalid');
+                }
+            }
+        )
+            .then(data => {
+                setFullName('Name: ' + data['firstname'] + ' ' + data['lastname']);
+                setUserName('Username: ' + data['userName']);
+                setEmail('Email: ' + data['email']);
+            })
+            .then(data => {
+                fetch('https://localhost:7280/api/vote/myvotes', {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Authorization': 'Bearer ' + authentic,
+                },
+                
+                // body: JSON.stringify(data)
+                })
+                .then(response => {
+                    if (response.status === 200) {
+                        console.log(response);
+                        // alert('Get request success');
+                        return response.json();
+                    } else {
+                        alert('Get request success invalid');
+                    }
+                })
+                .then(data => {
+                    setVotes(data.length);
+                })
+            })
+
     return (
         <div>
             <div class="profile-page">
@@ -14,21 +86,24 @@ function Profile() {
                 {/* <input class="picture" value="&#9786;"></input> */}
                 <div class="centerer">
                     <div class="firstname">
-                        Full Name
+                        {/* {fullName} */}
+                        {/* conditon: {cond() ? 'true' : 'false'} */}
+                        {fullName}
                     </div>
                 </div>
                 <div class="centerer">
                     <div class="username">
-                        Username
+                        {userName}
+                        
                     </div>
                 </div>
                 <div class="centerer">
                     <div class="email">
-                        Email
+                        {email}
                     </div>
                 </div>
                 <div class="bottom">
-                    <div class="votes">94,000 votes</div>
+                    <div class="votes">{votes} votes</div>
                     <div class="seperator">|</div>
                     <div class="polls">3,120 polls</div>
                 </div>
